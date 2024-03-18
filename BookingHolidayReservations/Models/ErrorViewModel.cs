426cf07f9e9aed68389986cb3c46da866ed03ea4
Asus <Asus@DESKTOP-GADMDLH>
2024-03-18 10:39:52 +0200

    Add project files.

---
+++ BookingHolidayReservations/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookingHolidayReservations.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
