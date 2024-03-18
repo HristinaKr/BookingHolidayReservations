eeb1b16ec23c56249be46b407b3e88b2098c1f29
hristina subchewa <x_xris_x@abv.bg>
2024-03-18 18:43:20 +0200

    MainMenuComponents

---
+++ BookingHolidayReservations/Controllers/Component.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookingHolidayReservations.Controllers
+{
+    public class Component : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return await Task.FromResult<IViewComponentResult>(View());
+        }
+    }
+}
