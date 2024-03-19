1444d13753feaddfa0d9940b129fdca4c1e5659c
hristina subchewa <x_xris_x@abv.bg>
2024-03-19 17:35:00 +0200

    User class added

---
+++ BookingHolidayReservations.Infrastructure/Data/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static BookingHolidayReservations.Infrastructure.Constants.ConstantsValidation;
+namespace BookingHolidayReservations.Infrastructure.Data.Models
+{
+    [Comment("User profile")]
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(UsernameLength)]
+        public string Username { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(EmailLength)]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(PasswordMaxLength)]
+        public string Password { get; set; } = string.Empty;
+
+    }
+}
