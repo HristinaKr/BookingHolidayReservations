08e6b31def65b47b36134284c4d310268bce4b75
hristina subchewa <x_xris_x@abv.bg>
2024-03-19 17:34:32 +0200

    Destination class added

---
+++ BookingHolidayReservations.Infrastructure/Data/Models/HolidayDestination.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using static BookingHolidayReservations.Infrastructure.Constants.ConstantsValidation;
+
+namespace BookingHolidayReservations.Infrastructure.Data.Models
+{
+    [Comment("Destination description")]
+    public class HolidayDestination
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(HolidayNameMaxLength)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(HolidayDescriptionLength)]
+        public string Description { get; set; } = string.Empty;
+
+        [Required]
+        public decimal PricePerPackage { get; set; }
+
+        [Required]
+        public string ImageUrl { get; set; } = string.Empty;
+
+
+    }
+}
