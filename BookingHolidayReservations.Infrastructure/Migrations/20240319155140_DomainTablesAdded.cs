e5cec9e9ecd368884745ff9c3f2333a01690d4c5
hristina subchewa <x_xris_x@abv.bg>
2024-03-19 18:59:51 +0200

    Migrations added

---
+++ BookingHolidayReservations.Infrastructure/Migrations/20240319155140_DomainTablesAdded.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookingHolidayReservations.Infrastructure.Migrations
+{
+    public partial class DomainTablesAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Admins",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    FullName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(15)", maxLength: 15, nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Admins", x => x.Id);
+                },
+                comment: "Admin functions");
+
+            migrationBuilder.CreateTable(
+                name: "ApplicationUsers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<string>(type: "nvarchar(60)", maxLength: 60, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(40)", maxLength: 40, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ApplicationUsers", x => x.Id);
+                },
+                comment: "User profile");
+
+            migrationBuilder.CreateTable(
+                name: "Bookings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    DestinationId = table.Column<int>(type: "int", nullable: false),
+                    CheckInDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CheckOutDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    NumberOfGuests = table.Column<int>(type: "int", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bookings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Bookings_ApplicationUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "ApplicationUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                },
+                comment: "Holiday bookings made by the user");
+
+            migrationBuilder.CreateTable(
+                name: "HolidayDestinations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    PricePerPackage = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BookingId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HolidayDestinations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_HolidayDestinations_Bookings_BookingId",
+                        column: x => x.BookingId,
+                        principalTable: "Bookings",
+                        principalColumn: "Id");
+                },
+                comment: "Destination description");
+
+            migrationBuilder.CreateTable(
+                name: "Payments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false, comment: "Payment information about the booking")
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BookingId = table.Column<int>(type: "int", nullable: false),
+                    TotalAmount = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    PaymentDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Payments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Payments_Bookings_BookingId",
+                        column: x => x.BookingId,
+                        principalTable: "Bookings",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_UserId",
+                table: "Bookings",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HolidayDestinations_BookingId",
+                table: "HolidayDestinations",
+                column: "BookingId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payments_BookingId",
+                table: "Payments",
+                column: "BookingId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Admins");
+
+            migrationBuilder.DropTable(
+                name: "HolidayDestinations");
+
+            migrationBuilder.DropTable(
+                name: "Payments");
+
+            migrationBuilder.DropTable(
+                name: "Bookings");
+
+            migrationBuilder.DropTable(
+                name: "ApplicationUsers");
+        }
+    }
+}
---
+++ BookingHolidayReservations.Infrastructure/Migrations/20240319165640_onDelete.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookingHolidayReservations.Infrastructure.Migrations
+{
+    public partial class onDelete : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bookings_ApplicationUsers_UserId",
+                table: "Bookings");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_HolidayDestinations_Bookings_BookingId",
+                table: "HolidayDestinations");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Payments_Bookings_BookingId",
+                table: "Payments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_HolidayDestinations_BookingId",
+                table: "HolidayDestinations");
+
+            migrationBuilder.DropColumn(
+                name: "BookingId",
+                table: "HolidayDestinations");
+
+            migrationBuilder.CreateTable(
+                name: "BookingHolidayDestination",
+                columns: table => new
+                {
+                    BookingsId = table.Column<int>(type: "int", nullable: false),
+                    DestinationId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookingHolidayDestination", x => new { x.BookingsId, x.DestinationId });
+                    table.ForeignKey(
+                        name: "FK_BookingHolidayDestination_Bookings_BookingsId",
+                        column: x => x.BookingsId,
+                        principalTable: "Bookings",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BookingHolidayDestination_HolidayDestinations_DestinationId",
+                        column: x => x.DestinationId,
+                        principalTable: "HolidayDestinations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookingHolidayDestination_DestinationId",
+                table: "BookingHolidayDestination",
+                column: "DestinationId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bookings_ApplicationUsers_UserId",
+                table: "Bookings",
+                column: "UserId",
+                principalTable: "ApplicationUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Payments_Bookings_BookingId",
+                table: "Payments",
+                column: "BookingId",
+                principalTable: "Bookings",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bookings_ApplicationUsers_UserId",
+                table: "Bookings");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Payments_Bookings_BookingId",
+                table: "Payments");
+
+            migrationBuilder.DropTable(
+                name: "BookingHolidayDestination");
+
+            migrationBuilder.AddColumn<int>(
+                name: "BookingId",
+                table: "HolidayDestinations",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HolidayDestinations_BookingId",
+                table: "HolidayDestinations",
+                column: "BookingId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bookings_ApplicationUsers_UserId",
+                table: "Bookings",
+                column: "UserId",
+                principalTable: "ApplicationUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_HolidayDestinations_Bookings_BookingId",
+                table: "HolidayDestinations",
+                column: "BookingId",
+                principalTable: "Bookings",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Payments_Bookings_BookingId",
+                table: "Payments",
+                column: "BookingId",
+                principalTable: "Bookings",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
