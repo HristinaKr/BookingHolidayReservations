eb8f411bcc18253b5b32b51ee08a1b70d0d150e5
hristina subchewa <x_xris_x@abv.bg>
2024-03-19 17:34:49 +0200

    Payment class added

---
+++ BookingHolidayReservations.Infrastructure/Data/Models/Payment.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingHolidayReservations.Infrastructure.Data.Models
+{
+    public class Payment
+    {
+        [Comment("Payment information about the booking")]
+        public int Id { get; set; }
+
+        [Required]
+        public int BookingId { get; set; }
+
+        [Required]
+        public decimal TotalAmount { get; set; }
+
+        [Required]
+        public DateTime PaymentDate { get; set; }
+
+        [ForeignKey(nameof(BookingId))]
+        public Booking Booking { get; set; } = new Booking();
+    }
+}
