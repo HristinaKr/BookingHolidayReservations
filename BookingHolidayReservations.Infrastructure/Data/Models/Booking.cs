2026603ec48c608805a7672fd2f9dc17059bd986
hristina subchewa <x_xris_x@abv.bg>
2024-03-19 17:34:18 +0200

    Booking class added

---
+++ BookingHolidayReservations.Infrastructure/Data/Models/Booking.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static BookingHolidayReservations.Infrastructure.Constants.ConstantsValidation;
+
+namespace BookingHolidayReservations.Infrastructure.Data.Models
+{
+    [Comment("Holiday bookings made by the user")]
+    public class Booking
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public int UserId { get; set; } 
+
+        [Required]
+        public int DestinationId { get; set; }
+
+        [Required]
+        public DateTime CheckInDate { get; set; }
+
+        [Required]
+        public DateTime CheckOutDate { get; set; }
+
+        [Required]
+        [MaxLength(GuestsMaxCount)]
+        public int NumberOfGuests { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        public User User { get; set; } = null!;
+
+        public List<HolidayDestination> Destination { get; set; } = new List<HolidayDestination>();
+    }
+}
