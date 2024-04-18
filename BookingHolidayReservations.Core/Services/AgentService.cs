9fed344a7f5557b3fb1cabe3da2e07a4bde14065
hristina subchewa <x_xris_x@abv.bg>
2024-04-18 10:02:34 +0300

    Add and SaveChanges methods added

---
+++ BookingHolidayReservations.Core/Services/AgentService.cs
@@ -0,0 +1,42 @@
+﻿using BookingHolidayReservations.Core.Contracts;
+using BookingHolidayReservations.Infrastructure.Data.Common;
+using BookingHolidayReservations.Infrastructure.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookingHolidayReservations.Core.Services
+{
+    public class AgentService : IAgentService
+    {
+        private readonly IRepository repository;
+        public AgentService(IRepository _repository)
+        {
+                repository = _repository;   
+        }
+
+        public Task CreateAsync(string userId, string phoneNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> ExistsById(string userId)
+        {
+            return await repository.AllReadOnly<Administrator>()
+                .AnyAsync(a => a.Id.ToString() == userId);
+        }
+
+        public Task<int?> GetAgentIdAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UserHasRentsAsync(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UserWithPhoneNumberExists(string phoneNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
